88fcd7b5bed8e559f5e700653aac59bf4d4757b7
Mike Hearn <mike@plan99.net>
2014-10-06 01:47:45 +0200

    Upgrade to new UpdateFX that supports much smaller patch sizes. Transition will take effect after next update.

--- package.sh
+++ package.sh
@@ -12,23 +12,12 @@ mvn -q -U clean package -DskipTests
 
 dest=updates/builds/$ver.jar
 
-#echo "Decompressing JAR for smaller updates"
-#mkdir client/target/repack
-#cd client/target/repack
-#unzip -q ../client-0.1-SNAPSHOT-bundled.jar
-#jar c0fm uncompressed.jar META-INF/MANIFEST.MF *
-#cd - >/dev/null
-#cp client/target/repack/uncompressed.jar $dest
-#rm -r client/target/repack
-
 cp client/target/client-0.1-SNAPSHOT-bundled.jar $dest
 echo "Copied build as version $ver to $dest"
 
-# TODO: Find a way to canonicalise timestamps to make small patches small.
-
 echo "Generating online update site"
 rm updates/site/* || true
-java -jar tools/updatefx.jar --url=https://s3-eu-west-1.amazonaws.com/vinumeris/lighthouse/updates updates
+java -jar tools/updatefx.jar --url=https://s3-eu-west-1.amazonaws.com/vinumeris/lighthouse/updates --gzip-from=7 updates
 
 if [[ $1 == "--package" ]]; then
     if [ -e /Library ]; then
