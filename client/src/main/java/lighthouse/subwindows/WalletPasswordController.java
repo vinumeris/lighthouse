84bbb7684756405f9afaf09fdb3b7791367c7262
Mike Hearn <mike@plan99.net>
2014-10-07 22:38:22 +0200

    Focus the password field.

--- client/src/main/java/lighthouse/subwindows/WalletPasswordController.java
+++ client/src/main/java/lighthouse/subwindows/WalletPasswordController.java
@@ -1,5 +1,6 @@
 package lighthouse.subwindows;
 
+import javafx.application.Platform;
 import org.bitcoinj.crypto.KeyCrypterScrypt;
 import com.google.common.primitives.Longs;
 import com.google.protobuf.ByteString;
@@ -39,6 +40,7 @@ public class WalletPasswordController {
 
     public void initialize() {
         progressMeter.setOpacity(0);
+        Platform.runLater(pass1::requestFocus);
     }
 
     @FXML void confirmClicked(ActionEvent event) {
