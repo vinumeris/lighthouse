93aa87a1593c94758d0dcfcc0ccc117717cfc2cb
Mike Hearn <mike@plan99.net>
2015-03-29 21:08:54 +0200

    i18n: Fix the case where a translation is missing and it's loaded via FXML with a custom ResourceBundle that never throws an error, it always returns the key (English string) if a real translation is missing.

--- client/src/main/java/lighthouse/utils/I18nUtil.java
+++ client/src/main/java/lighthouse/utils/I18nUtil.java
@@ -1,15 +1,44 @@
 package lighthouse.utils;
 
+import org.jetbrains.annotations.*;
 import org.slf4j.*;
 
-import javax.annotation.*;
+import javax.annotation.Nullable;
 import java.util.*;
 
 import static gnu.gettext.GettextResource.*;
 
 public class I18nUtil {
     private static final Logger log = LoggerFactory.getLogger(I18nUtil.class);
-    @Nullable public static ResourceBundle locale;
+    @Nullable private static ResourceBundle locale;
+
+    public static ResourceBundle translations = new ResourceBundle() {
+        @Override
+        protected Object handleGetObject(String key) {
+            if (locale == null)
+                return key;
+            else
+                try {
+                    return locale.getObject(key);
+                } catch (MissingResourceException e) {
+                    return key;
+                }
+        }
+
+        @Override
+        public boolean containsKey(String key) {
+            return true;
+        }
+
+        @NotNull
+        @Override
+        public Enumeration<String> getKeys() {
+            if (locale != null)
+                return locale.getKeys();
+            else
+                return Collections.emptyEnumeration();
+        }
+    };
 
     static {
         try {
